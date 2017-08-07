3d8dcc6ca456665fbcc3d352283d4e69c43e6089
therzv <rizvi.area@gmail.com>
2017-08-07 05:17:54 -0700

    ketiga

--- building.go
+++ building.go
@@ -4,4 +4,5 @@ import "github.com/therzv/mygo/floor"
 
 func main() {
 	floor.ShowAdultCompany("test1", "test2", "test3", "test4", "test5")
+	floor.ShowEcommerceCompany("MatahariMall", "Tokopedia", "Bukalapak")
 }
