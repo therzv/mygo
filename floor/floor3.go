183a12c78b8ed17e933df04baae62bc58ca9432c
therzv <rizvi.area@gmail.com>
2017-08-08 21:53:39 -0700

    forloop

--- floor/floor3.go
+++ floor/floor3.go
@@ -6,7 +6,9 @@ import (
 
 func ShowAdultCompany(s string, n int) {
 	for i := 0; i < n; i++ {
-		n += i
+		fmt.Println(s, i)
+		//a := fmt.Sprintf("ini rizvi punya string s %s ke %d ", s, i)
+		//fmt.Println(a)
 	}
-	fmt.Println(s)
+
 }
